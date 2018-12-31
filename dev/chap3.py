0e7e9d2ffbbeb1e174991b61e697d97da378e909
Idris <idris.benkhelil@gmail.com>
2018-12-31 14:00:54 +0100

    chap3

---
+++ dev/chap3.py
@@ -0,0 +1,63 @@
+import numpy as np
+import os
+import matplotlib
+import matplotlib.pyplot as plt
+
+
+def sort_by_target(mnist):
+    reorder_train = np.array(sorted([(target, i)
+                                     for i, target in enumerate(mnist.target[:60000])]))[:, 1]
+    reorder_test = np.array(sorted([(target, i)
+                                    for i, target in enumerate(mnist.target[60000:])]))[:, 1]
+    mnist.data[:60000] = mnist.data[reorder_train]
+    mnist.target[:60000] = mnist.target[reorder_train]
+    mnist.data[60000:] = mnist.data[reorder_test + 60000]
+    mnist.target[60000:] = mnist.target[reorder_test + 60000]
+
+
+try:
+    from sklearn.datasets import fetch_openml
+    mnist = fetch_openml('mnist_784', version=1, cache=True)
+    mnist.target = mnist.target.astype(np.int8)  # fetch_openml() returns targets as strings
+    sort_by_target(mnist)  # fetch_openml() returns an unsorted dataset
+except ImportError:
+    from sklearn.datasets import fetch_mldata
+    mnist = fetch_mldata('MNIST original')
+
+mnist["data"], mnist["target"]
+
+X, y = mnist["data"], mnist["target"]
+
+
+some_digit = X[36000]
+some_digit_image = some_digit.reshape(28, 28)
+some_digit_image
+
+plt.imshow(some_digit_image, cmap=matplotlib.cm.binary, interpolation="nearest")
+plt.axis("off")
+
+y[36000]
+X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
+
+y_train_5 = (y_train == 5)  # True for all 5s, False for all other digits.
+y_test_5 = (y_test == 5)
+
+from sklearn.linear_model import SGDClassifier
+sgd_clf = SGDClassifier(random_state=42)
+sgd_clf.fit(X_train, y_train_5)
+
+sgd_clf.predict([some_digit])
+
+from sklearn.model_selection import cross_val_score
+cross_val_score(sgd_clf, X_train, y_train_5, cv=3, scoring="accuracy")
+
+
+from sklearn.base import BaseEstimator
+
+
+class Never5Classifier(BaseEstimator):
+    def fit(self, X, y=None):
+        pass
+
+    def predict(self, X):
+        return np.zeros((len(X), 1), dtype=bool)
