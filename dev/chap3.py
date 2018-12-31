19f412ab020e94015d47e136b9fa6906f5af533f
Idris <idris.benkhelil@gmail.com>
2018-12-31 14:07:05 +0100

    maj

--- dev/chap3.py
+++ dev/chap3.py
@@ -5,10 +5,8 @@ import matplotlib.pyplot as plt
 
 
 def sort_by_target(mnist):
-    reorder_train = np.array(sorted([(target, i)
-                                     for i, target in enumerate(mnist.target[:60000])]))[:, 1]
-    reorder_test = np.array(sorted([(target, i)
-                                    for i, target in enumerate(mnist.target[60000:])]))[:, 1]
+    reorder_train = np.array(sorted([(target, i) for i, target in enumerate(mnist.target[:60000])]))[:, 1]
+    reorder_test = np.array(sorted([(target, i) for i, target in enumerate(mnist.target[60000:])]))[:, 1]
     mnist.data[:60000] = mnist.data[reorder_train]
     mnist.target[:60000] = mnist.target[reorder_train]
     mnist.data[60000:] = mnist.data[reorder_test + 60000]
@@ -43,7 +41,7 @@ y_train_5 = (y_train == 5)  # True for all 5s, False for all other digits.
 y_test_5 = (y_test == 5)
 
 from sklearn.linear_model import SGDClassifier
-sgd_clf = SGDClassifier(random_state=42)
+sgd_clf = SGDClassifier(max_iter=5, tol=-np.infty, random_state=42)
 sgd_clf.fit(X_train, y_train_5)
 
 sgd_clf.predict([some_digit])
@@ -51,6 +49,9 @@ sgd_clf.predict([some_digit])
 from sklearn.model_selection import cross_val_score
 cross_val_score(sgd_clf, X_train, y_train_5, cv=3, scoring="accuracy")
 
+
+X_train.shape
+y_train_5.shape
 
 from sklearn.base import BaseEstimator
 
@@ -61,3 +62,20 @@ class Never5Classifier(BaseEstimator):
 
     def predict(self, X):
         return np.zeros((len(X), 1), dtype=bool)
+
+
+never_5_clf = Never5Classifier()
+
+cross_val_score(never_5_clf, X_train, y_train_5, cv=3, scoring="accuracy").mean()
+
+from sklearn.model_selection import cross_val_predict
+y_train_pred = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3)
+y_train_pred.shape
+
+from sklearn.metrics import confusion_matrix
+confusion_matrix(y_train_5, y_train_pred)
+
+from sklearn.metrics import precision_score, recall_score
+
+precision_score(y_train_5, y_train_pred)
+recall_score(y_train_5, y_train_pred)
