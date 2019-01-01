a27b2add823519bb848fa043f623f5499cc7480f
idris <idris.benkhelil@gmail.com>
2019-01-01 13:17:38 +0100

    3

--- dev/chap3.py
+++ dev/chap3.py
@@ -1,3 +1,10 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import precision_score, recall_score, f1_score, roc_auc_score
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import cross_val_predict
+from sklearn.base import BaseEstimator
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import SGDClassifier
 import numpy as np
 import os
 import matplotlib
@@ -5,8 +12,10 @@ import matplotlib.pyplot as plt
 
 
 def sort_by_target(mnist):
-    reorder_train = np.array(sorted([(target, i) for i, target in enumerate(mnist.target[:60000])]))[:, 1]
-    reorder_test = np.array(sorted([(target, i) for i, target in enumerate(mnist.target[60000:])]))[:, 1]
+    reorder_train = np.array(sorted([(target, i)
+                                     for i, target in enumerate(mnist.target[:60000])]))[:, 1]
+    reorder_test = np.array(sorted([(target, i)
+                                    for i, target in enumerate(mnist.target[60000:])]))[:, 1]
     mnist.data[:60000] = mnist.data[reorder_train]
     mnist.target[:60000] = mnist.target[reorder_train]
     mnist.data[60000:] = mnist.data[reorder_test + 60000]
@@ -40,20 +49,16 @@ X_train, X_test, y_train, y_test = X[:60000], X[60000:], y[:60000], y[60000:]
 y_train_5 = (y_train == 5)  # True for all 5s, False for all other digits.
 y_test_5 = (y_test == 5)
 
-from sklearn.linear_model import SGDClassifier
 sgd_clf = SGDClassifier(max_iter=5, tol=-np.infty, random_state=42)
 sgd_clf.fit(X_train, y_train_5)
 
 sgd_clf.predict([some_digit])
 
-from sklearn.model_selection import cross_val_score
 cross_val_score(sgd_clf, X_train, y_train_5, cv=3, scoring="accuracy")
 
 
 X_train.shape
 y_train_5.shape
-
-from sklearn.base import BaseEstimator
 
 
 class Never5Classifier(BaseEstimator):
@@ -68,14 +73,24 @@ never_5_clf = Never5Classifier()
 
 cross_val_score(never_5_clf, X_train, y_train_5, cv=3, scoring="accuracy").mean()
 
-from sklearn.model_selection import cross_val_predict
 y_train_pred = cross_val_predict(sgd_clf, X_train, y_train_5, cv=3)
 y_train_pred.shape
 
-from sklearn.metrics import confusion_matrix
 confusion_matrix(y_train_5, y_train_pred)
 
-from sklearn.metrics import precision_score, recall_score
 
 precision_score(y_train_5, y_train_pred)
 recall_score(y_train_5, y_train_pred)
+f1_score(y_train_5, y_train_pred)
+roc_auc_score(y_train_5, y_train_pred)
+
+
+rfe = RandomForestClassifier(n_estimators=10, random_state=42)
+y_proba_forest = cross_val_predict(rfe, X_train, y_train_5, cv=3, method='predict_proba')
+y_proba_forest
+y_score_forest = y_proba_forest[:, 1]
+roc_auc_score(y_train_5, y_score_forest)
+y_train_pred_forest = cross_val_predict(rfe, X_train, y_train_5, cv=3)
+precision_score(y_train_5, y_train_pred_forest)
+recall_score(y_train_5, y_train_pred_forest)
+f1_score(y_train_5, y_train_pred_forest)
